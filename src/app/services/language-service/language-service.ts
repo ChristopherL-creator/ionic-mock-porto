0df9e99a3c1c2c7a1e9718214723def5565675b9
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-07-20 17:48:29 +0200

    Update language-service.ts

--- src/app/services/language-service/language-service.ts
+++ src/app/services/language-service/language-service.ts
@@ -9,9 +9,10 @@ export class LanguageService {
   constructor(private translateServ: TranslateService) { }
 
   languageChange(language: string){
-    // console.log(language);
     this.translateServ.use(language);
+//  per salvare configuarazione
     localStorage.setItem('myConfig', language);
+// per ricaricare app
     window.location.reload();
   }
 }
